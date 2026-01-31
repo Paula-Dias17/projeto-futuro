64cd9db9230658a807503b8a6cda5974a65f3093
Paula-Dias17 <m.paula3405@gmail.com>
2026-01-31 20:13:10 -0300

    terminei o obras e iniciei o compras

--- js/java.js
+++ js/java.js
@@ -36,3 +36,64 @@ numeros.forEach(contato => {
         contato.classList.remove('hovered');
     });
 });
+
+
+//pedrina
+// const imgs = ["./img/perfil_paula2 (1)", "./img/perfil_paula2 (2)"]
+// let pos = 0;
+// const carrossel = document.querySelector("#perfil");
+// anterior = () =>{
+//     if (pos - 1 >= 0){
+//         pos = pos - 1;
+//         url_img = imgs[pos]
+//         carrossel.attributes[1].value = url_img;
+//     } 
+//     carrossel.src = imgs[pos];
+//     contador.innerHTML = `${pos + 1}/${imgs.length}`;
+// }
+
+// proxima = () =>{
+//     if (pos + 1 > imgs.length-1){
+//         pos = 0
+//     }else{
+//         pos = pos + 1;                
+//     }
+//     url_img = imgs[pos]
+//     carrossel.attributes[1].value = url_img;
+//     contador.innerHTML = `${pos+1}/${imgs.length}`;
+// }
+
+// setInterval(() => {
+//     proxima();
+// }, 3000); 
+
+//chat
+const imgs = [
+  "./img/perfil_paula2 (1).jpeg",
+  "./img/perfil_paula2 (2).jpeg"
+];
+
+let pos = 0;
+const carrossel = document.querySelector("#perfil");
+
+anterior = () => {
+    if (pos - 1 >= 0) {
+        pos--;
+    } else {
+        pos = imgs.length - 1;
+    }
+
+    carrossel.src = imgs[pos];
+    contador.innerHTML = `${pos + 1}/${imgs.length}`;
+}
+
+proxima = () => {
+    pos = (pos + 1) % imgs.length;
+    carrossel.src = imgs[pos];
+    contador.innerHTML = `${pos + 1}/${imgs.length}`;
+}
+
+// ⏱️ troca automática
+setInterval(() => {
+    proxima();
+}, 3000);
